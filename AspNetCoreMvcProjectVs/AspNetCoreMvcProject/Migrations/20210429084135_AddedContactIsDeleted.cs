69da5a0d775975386d20c54e73fbd6dbe68df278
sarvankm <sarvankm@code.edu.az>
2021-04-29 12:44:20 +0400

    Some Problems Fixed

---
+++ AspNetCoreMvcProjectVs/AspNetCoreMvcProject/Migrations/20210429084135_AddedContactIsDeleted.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreMvcProject.Migrations
+{
+    public partial class AddedContactIsDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Contacts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Contacts");
+        }
+    }
+}
